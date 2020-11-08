8c9fb7ed243d7308d8526f93e9ad80698c93eff2
Finxx1 <robth3gam33r@outlook.com>
2020-11-07 20:27:08 -0600

    Added Support for P455C0D35

--- DAT2BAT.py
+++ DAT2BAT.py
@@ -12,9 +12,18 @@ if len(sys.argv) < 2:  # Make sure that people actually give the dang script fil
     input('Press enter to exit')
 else:
 
+    usePass = 0  # Determine whether to use a password. Right here so the for loop can reach it without overriding it
+
     archivename = os.path.splitext(sys.argv[1])[0] + ".bat"
 
     for x in range(len(sys.argv) - 1):
+        # Determine whether we use passwords
+        if sys.argv[x + 1] == "-pass":
+            usePass = 1
+            archivename = os.path.splitext(sys.argv[3])[0] + ".bat"
+            continue
+        if usePass == 1 and x + 1 == 2:
+            continue
         # Read
         filename = sys.argv[x + 1]  # Get file
         filestream = open(filename, "rb")  # Create a file stream
@@ -32,4 +41,7 @@ else:
         # Split Base64 data at every 8000th character so a cmd line doesnt get too long
         nfiledata = insert_data(str(nfiledata), " >> temp\r\necho ", 8000)
         # Format data into batch using magic
-        nfilestream.write("echo " + str(nfiledata).replace("b'", "").replace("'", "") + " >> temp\r\ncertutil -decode temp " + os.path.split(filename)[1] + "\r\ndel temp -yr\r\n")
+        if usePass == 1:
+            nfilestream.write("@echo off\r\n:wrong\r\nset /p psw=Password? : \r\nif %psw% == " + sys.argv[2] + " goto :correct\r\ngoto :wrong\r\n:correct\r\necho " + str(nfiledata).replace("b'", "").replace("'", "") + " >> temp\r\ncertutil -decode temp " + os.path.split(filename)[1] + "\r\ndel temp -y\r\n:end\r\n")
+        else:
+            nfilestream.write("echo " + str(nfiledata).replace("b'", "").replace("'", "") + " >> temp\r\ncertutil -decode temp " + os.path.split(filename)[1] + "\r\ndel temp -y")
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A data archiving utility
 ## How do I use it?
 ### Prerequisites
-Install python [here](python.org/downloads), making sure to select Add Python (Version) to PATH.
+Install python [here](https://www.python.org/downloads/), making sure to select Add Python (Version) to PATH.
 ### Method 1.
 1. Right-click the DAT2BAT.py file and click properties
 2. Press the button that says ```CHANGE```
@@ -16,5 +16,10 @@ Install python [here](python.org/downloads), making sure to select Add Python (V
 3. Type ```py DAT2BAT.py <file(s)>```
 ---
 If you followed the steps correctly, you will end up with a batch script that can work even without DAT2BAT, making it perfect for sharing multiple files with other people in an easy and efficient way.
+
+## Passwords
+
+To make an archive require a password, you have to use the command line. Typing ```py DAT2BAT.py -pass <password> <file(s)>``` will give you an archive that requires a password to open.
+
 ## How does it work?
 The Python script takes in any number of files and outputs a batch script. The batch script works by echo-ing the Base64 data of the files archived by DAT2BAT into a file called ```temp```. Then, we call ```certutil -decode```. This program comes bundled with windows 7, 8, 8.1, and 10. It decodes the data and outputs it as a file with the same name as the archived file
